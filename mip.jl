e0e930b7381ec1de8ffcef379b4254db8ffae84c
loladekhuijzen <llgdekhuijzen@gmail.com>
2026-03-09 16:33:38 +0100

    mip using warm start

--- mip.jl
+++ mip.jl
@@ -19,6 +19,7 @@ M = length(facilities)
 println(typeof(facilities_col))
 locations = Dict{Int, Vector{Int}}()
 
+
 for k in 1:N
     i = clients_col[k]
     if haskey(locations, i)
@@ -49,58 +50,92 @@ end
 
 
 total_time = sum((0.2 * t_ij_col[k]) * (population[clients_col[k]+1]*0.1) for k in 1:N)
-total_pop = sum(population)*0.1
-avg_time = total_time / total_pop
-estimated_cost = avg_time * (total_pop / M) # cost of a school equals average travel time (cost) of students per school
-facility_cost = 2 * estimated_cost * 200 # per year
+facility_cost = (2 * total_time * 200) / M
 
+min_students = 100
+λ = facility_cost / (min_students / 2)
 
-factor = 0.001
-open = falses(M)
-
-λ = factor * facility_cost
 
 model = Model(HiGHS.Optimizer)
-# set_time_limit_sec(model, 60)
-set_optimizer_attribute(model, "mip_rel_gap", 0.05)
+
+set_optimizer_attribute(model, "mip_rel_gap", 0.01)
 set_optimizer_attribute(model, "presolve", "on")
+set_optimizer_attribute(model, "user_objective_scale", -1)
 
-# variables
-@variable(model, y[1:N], Bin)    # per od pair
-@variable(model, x[j in facilities], Bin)   # per facility
 
-# objective
-@objective(model, Min, sum(y[k] * t_ij_col[k] * population[clients_col[k]+1]*0.1 for k in 1:N) + sum(x[j]*λ for j in facilities))
+@variable(model, 0 <= y[1:N] <= 1)
+@variable(model, 0 <= x[j in facilities] <= 1)
+@variable(model, deficit[j in facilities] >= 0)
 
-# constraints
+
+
+@expression(model, load[j in facilities],
+    sum(y[k] * population[clients_col[k]+1] * 0.1
+        for k in facility_rows[j])
+)
+
+
+@objective(model, Min, sum(y[k] * t_ij_col[k] * population[clients_col[k]+1] * 0.1 for k in 1:N) + sum(deficit[j] * λ for j in facilities))
+
+
+# each client assigned
 for (_, rows) in locations
     @constraint(model, sum(y[k] for k in rows) == 1)
 end
 
+# only assign to open facility
 for k in 1:N
     @constraint(model, y[k] <= x[facilities_col[k]])
 end
 
-optimize!(model)
-
-# for k in 1:N
-#     if value(y[k]) > 0.5
-#         println("client $(locations_col[k]) assigned to facility $(facilities_col[k])")
-#     end
-# end
-                                                                                                        
-println(sum(value(x[j]) for j in facilities), " facilities open")                       
-    
+# deficit definition
 for j in facilities
-    open[j+1] = value(x[j]) > 0.5
+    @constraint(model, deficit[j] >= min_students * x[j] - load[j])
 end
 
 
-columns = Dict{Symbol, AbstractVector}()
-columns[:id] = facilities
-columns[:open] = open
+println("solving LP relaxation...")
+optimize!(model)
+
+# store relaxed solution
+x_relaxed = value.(x)
+y_relaxed = value.(y)
 
 
-Arrow.write("C:\\LocalData\\networkmodel_eu\\$(country)_j_mip.arrow", (
-    columns
-))
+println("converting to MIP...")
+set_binary.(x)
+
+
+# fix integer values
+tol = 1e-6
+for j in facilities
+    if abs(x_relaxed[j]) <= tol
+        fix(x[j], 0.0; force=true)
+    elseif abs(x_relaxed[j] - 1.0) <= tol
+        fix(x[j], 1.0; force=true)
+    end
+end
+
+
+for j in facilities
+    set_start_value(x[j], x_relaxed[j])
+end
+
+for k in 1:N
+    set_start_value(y[k], y_relaxed[k])
+end
+
+# optional rounding 
+for j in facilities
+    if x_relaxed[j] > 0.5
+        set_start_value(x[j], 1.0)
+    else
+        set_start_value(x[j], 0.0)
+    end
+end
+
+
+println("solving MIP with warm start...")
+optimize!(model)
+
+println("objective value: ", objective_value(model))
